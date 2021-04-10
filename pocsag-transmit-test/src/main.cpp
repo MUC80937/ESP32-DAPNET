28a8af35e8063324bd717260b636429cd00025de
catSIXe <github2.konrad@sch.umann.it>
2021-04-10 16:39:23 +0200

    v1.0 of the code

--- pocsag-transmit-test/src/main.cpp
+++ pocsag-transmit-test/src/main.cpp
@@ -16,9 +16,10 @@
 
 SX1276 radio = new Module(LORA_SS, LORA_DIO0, LORA_RST, LORA_DIO1);
 POCSAGTransmitter transmitter;
+
 void setup() {
   Serial.begin(115200);
-  int state = radio.beginFSK(434.230,1.2,4.5);
+  int state = radio.beginFSK(868.23,1.2,4.5);
   if (state == ERR_NONE) {
     Serial.println(F("success!"));
   } else {
@@ -26,12 +27,17 @@ void setup() {
     Serial.println(state);
     while (true);
   }
-  //int state2 = radio.transmit("Hello World!");
   Serial.println("initialized modem");
   transmitter.begin(&radio);
   Serial.println("initialized transmitter");
   delay(2e2);
-  transmitter.test();
+  transmitter.queuePage(133701, 3, "Testbericht 0"); // should never be sent
+  transmitter.clearQueue();
+  transmitter.queuePage(133701, 3, "Testbericht 1");
+  transmitter.queuePage(133703, 3, "Testbericht -2");
+  transmitter.queuePage(133706, 3, "Testbericht --3");
+  transmitter.queuePage(133707, 3, "Testbericht ---4");
+  transmitter.transmitBatch();
 }
 
 void loop() {
--- pocsag-transmit-test/src/pocsag_transmitter.h
+++ pocsag-transmit-test/src/pocsag_transmitter.h
@@ -1,23 +1,50 @@
 #if !defined(_POCSAG_TRANSMITTER_H)
 #define _POCSAG_TRANSMITTER_H
 #include <RadioLib.h>
-// #include <SX1278.h>
+
+#define POCSAG_MAX_ENCODING_BUFFER_SIZE 70 // 70 QBYTEs should be enough for max 80char Messages
+#define POCSAG_MAX_BUFFER_SIZE 2000 // 2000 QBYTEs
+#define POCSAG_PIN_DIO1 33 // DCLK
+#define POCSAG_PIN_DIO2 32 // DATA
+
+// #define POCSAG_DEBUG
+#define POCSAG_DEBUG_PORT   Serial
+#if defined(POCSAG_DEBUG)
+  #define POCSAG_DEBUG_PRINT(...) { RADIOLIB_DEBUG_PORT.print(__VA_ARGS__); }
+  #define POCSAG_DEBUG_PRINTLN(...) { RADIOLIB_DEBUG_PORT.println(__VA_ARGS__); }
+#else
+  #define POCSAG_DEBUG_PRINT(...) {}
+  #define POCSAG_DEBUG_PRINTLN(...) {}
+#endif
 
 /*!
 * POCSAG Transmitter
-- catSIXe
+- catSIXe, encoder is partly transformed from RPITX, with my old contributions(batch-encoding etc.) from the old github
 */
+
+static SX1278* _currentRadio; // we need to put this pointer static for the ISR
+static uint32_t transmitterData[ POCSAG_MAX_BUFFER_SIZE ]; // complete transmission
+static uint32_t encodingBuffer[ POCSAG_MAX_ENCODING_BUFFER_SIZE ]; // small static allocated memory, for storing a encoded chunk
+
+static uint16_t transmitterLength;
+static uint16_t transmitterOffset;
+static uint8_t transmitterBitOffset;
+static bool transmitterState;
+static void IRAM_ATTR onTransmitterClock();
+
 class POCSAGTransmitter {
     public:
-        uint32_t *transmitterData;
-        uint32_t *encodingBuffer;
+        SX1278* _radio;
         explicit POCSAGTransmitter();
         void begin(SX1278 *radio);
-        void queuePage(uint32_t ric, char* text);
-        void transmitBatch();
+        void clearQueue();
+        int8_t getQueueCount();
+        bool isActive();
+        void queuePage(uint32_t ric, uint8_t functionBit, char* text);
+        bool transmitBatch();
         void test();
-        void writeFIFOChunk(uint8_t* data, size_t len);
-    SX1278* _radio;
+    private:
+        int8_t queueCount = 0;
 };
 
 #endif
